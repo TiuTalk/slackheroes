e755f484ef4fdd22ec775bfc74b5b2a837da73e6
Thiago Belem <contato@thiagobelem.net>
2016-10-23 20:13:20 -0200

    Display at least 5 decks per team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,6 +2,14 @@ class TeamsController < ApplicationController
   def show
     @team = Team.find_by_domain!(params[:id])
 
-    render 'loading' if @team.user_reactions.count < 100
+    if @team.user_reactions.count >= 100
+      @decks = RankingDeck::EMOJI_LIST.keys.shuffle.map do |deck|
+        RankingDeck.new(deck, team: @team)
+      end.reject do |deck|
+        deck.cards.empty?
+      end
+    else
+      render 'loading'
+    end
   end
 end
