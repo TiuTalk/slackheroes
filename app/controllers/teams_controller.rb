638b07d236ec406f6653932f6f76a66b2607e60a
Thiago Belem <contato@thiagobelem.net>
2016-10-23 20:05:24 -0200

    Add loading information

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,7 @@
 class TeamsController < ApplicationController
   def show
     @team = Team.find_by_domain!(params[:id])
+
+    render 'loading' if @team.user_reactions.count < 100
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def emoji_url(emoji, team)
+  def emoji_url(emoji, team = nil)
     url = if image_filename = Emoji.find_by_alias(emoji).try(:image_filename)
             "/images/emoji/#{image_filename}"
           end
