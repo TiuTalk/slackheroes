682b07fdce623d617818c71bfe7262002e2595aa
Thiago Belem <contato@thiagobelem.net>
2016-10-23 12:03:22 -0200

    Home redirect to a random team

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    @team = Team.order('RANDOM()').first
+
+    redirect_to(@team) if @team.present?
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-
   describe "GET #home" do
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
+    context 'without a team' do
+      it "returns http success" do
+        get :home
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'with a team' do
+      let!(:team) { create(:team) }
+
+      it 'redirect to the team page' do
+        get :home
+        expect(response).to redirect_to(team)
+      end
     end
   end
-
 end
