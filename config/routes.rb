6f07675b0880f9d524436fe8120ba5b833dad4bc
Vinícius Oliveira <me@vinioliveira.com>
2016-10-22 22:08:07 -0200

    Adding Sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Sidekiq::Web, at: '/sidekiq'
   root 'pages#home'
   get '/auth/:provider/callback', to: 'authorizations#authorize'
   get '/authorize', to: 'authorizations#authorize'
