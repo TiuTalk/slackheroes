426358dffc78e387eeee4488fe0968882e4e5516
Vinícius Oliveira <me@vinioliveira.com>
2016-10-23 16:14:52 -0200

    Filter CustomEmojis by team

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def emoji_url(emoji)
+  def emoji_url(emoji, team)
     if url = Emoji.find_by_alias(emoji).try(:image_filename)
       "/images/emoji/#{url}"
     else
-      CustomEmoji.find_by_name(emoji).try(:url)
+      CustomEmoji.find_by(name: emoji, team: team).try(:url)
     end
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,18 +2,25 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper do
   describe '#emoji_url' do
+    let(:team) { create(:team, name: 'TEAM') }
     it 'returns emojis defaults img tag' do
-      expect(helper.emoji_url('smile')).to \
+      expect(helper.emoji_url('smile', team)).to \
         be_eql("/images/emoji/unicode/1f604.png")
     end
 
     it 'returns custom emojis from img tag' do
-      create(:custom_emoji, name: 'lol', url: 'http://google.com/image/lol')
-      expect(helper.emoji_url('lol')).to be_eql("http://google.com/image/lol")
+      create(:custom_emoji, name: 'lol', url: 'http://google.com/image/lol', team: team)
+      expect(helper.emoji_url('lol', team)).to be_eql("http://google.com/image/lol")
+    end
+
+    it 'returns custom emojis from img tag for the right team' do
+      create(:custom_emoji, name: 'lol', url: 'http://google.com/image/lol', team: team)
+      create(:custom_emoji, name: 'lol', url: 'http://google.com/image/lol2')
+      expect(helper.emoji_url('lol', team)).to be_eql("http://google.com/image/lol")
     end
 
     it 'returns nil when does not exists' do
-      expect(helper.emoji_url('aaa')).to be_nil
+      expect(helper.emoji_url('aaa', team)).to be_nil
     end
   end
 end
