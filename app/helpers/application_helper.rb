9d4f77715ac84d6971fcab0d1c0fd58ee3090bf2
Anezio Campos <newdevas@gmail.com>
2016-10-23 20:25:29 -0200

    Add fallback to emoji loading

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def emoji_url(emoji, team = nil)
-    url = if image_filename = Emoji.find_by_alias(emoji).try(:image_filename)
-            "/images/emoji/#{image_filename}"
-          end
-     url ||= CustomEmoji.find_by(name: emoji, team: team).try(:url)
-     url || emoji
+    image_filename = Emoji.find_by_alias(emoji).try(:image_filename)
+    url = "/images/emoji/#{image_filename}" if image_filename.present?
+    url ||= CustomEmoji.find_by(name: emoji, team: team).try(:url)
+    url ||= CustomEmoji.find_by(name: emoji).try(:url)
+    url || emoji
   end
 end
